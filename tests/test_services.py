e7b248c2fe2b473c77f3e0e1f74cca00768096ae
Natawut Nupairoj <natawut.n@chula.ac.th>
2020-10-04 10:49:30 +0700

    Version 1.2.1 - Fix test_services.py by starting todo-service in the script

--- tests/test_services.py
+++ tests/test_services.py
@@ -2,17 +2,27 @@
 # Natawut Nupairoj
 # Department of Computer Engineering, Chulalongkorn University
 # Created for 2110415 Software Defined Systems
+import logging
+import os
+import sys
+
+import pytest
+from xprocess import ProcessStarter
+
 from datetime import datetime
 import requests
 
+
+# global process variable
 service_url = 'http://localhost:8000/'
 
 
-def test_connectivity():
+def test_connectivity(server):
     resp = requests.get(service_url)
     assert resp.status_code == 200
 
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_post_and_get():
     resp = requests.get(service_url)
     existing = len(resp.json())
@@ -36,3 +46,25 @@ def test_post_and_get():
     assert resp.status_code == 200
     assert len(resp.json()) == (existing + n)
 
+
+@pytest.fixture(scope='session')
+def logger():
+    log = logging.getLogger(__name__)
+    return log
+
+
+@pytest.fixture(autouse=True, scope='session')
+def server(xprocess, logger):
+    python_executable_full_path = sys.executable
+    base_path = os.path.dirname(os.path.dirname(__file__))
+    app_path = os.path.join(base_path, 'todo')
+    logger.debug('using app path: ' + app_path)
+
+    class Starter(ProcessStarter):
+        pattern = 'startup complete'
+        arg_str = '-m uvicorn main:app --app-dir {} --host=0.0.0.0 --port=8000'.format(app_path)
+        args = [python_executable_full_path] + arg_str.split()
+
+    logfile = xprocess.ensure('server', Starter)
+    yield
+    xprocess.getinfo("server").terminate()
