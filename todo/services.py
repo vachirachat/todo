868ad7ba286f8ab24bb02ccd090c10e69f8ee1dc
Natawut Nupairoj <natawut.n@chula.ac.th>
2020-10-12 06:44:02 +0700

    Version 3.0 - Call notification service to add future notification events

--- todo/services.py
+++ todo/services.py
@@ -2,6 +2,8 @@
 # Natawut Nupairoj
 # Department of Computer Engineering, Chulalongkorn University
 # Created for 2110415 Software Defined Systems
+import requests
+import os
 
 from typing import List
 from fastapi import APIRouter, HTTPException
@@ -36,10 +38,34 @@ async def get_todo(id: int):
     raise HTTPException(status_code=404, detail="Todo with given id not found")
 
 
+if 'NOTIFICATION_HOST' in os.environ:
+    notification_host = os.environ['NOTIFICATION_HOST']
+else:
+    notification_host = 'localhost'
+if 'NOTIFICATION_PORT' in os.environ:
+    notification_port = os.environ['NOTIFICATION_PORT']
+else:
+    notification_port = 9000
+
+
+def set_notification(todo):
+    pass
+    url = 'http://{}:{}/'.format(notification_host, notification_port)
+    data = {
+        'id': todo['id'],
+        'title': todo['title'],
+        'timestamp': int(todo['duedate'].timestamp())
+    }
+    print(data)
+    requests.post(url, json=data)
+
+
 @todo_services.post('/', status_code=201)
 async def add_todo(payload: Todo):
     todo = payload.dict()
     id = db.add(todo)
+    # we also have to set notification
+    set_notification(todo)
     return {'id': id}
 
 
